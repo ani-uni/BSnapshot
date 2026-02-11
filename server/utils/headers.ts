91fd5289ca25a6e83be3bf484542204c7e07cad9
rinne <180847792+rinnein@users.noreply.github.com>
2026-02-11 13:57:36 +0800

    feat: update headers

--- server/utils/headers.ts
+++ server/utils/headers.ts
@@ -1,5 +1,6 @@
 export const headers: Headers = new Headers({
   'User-Agent':
-    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.82',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36',
   Referer: 'https://www.bilibili.com/',
+  Origin: 'https://www.bilibili.com',
 })
