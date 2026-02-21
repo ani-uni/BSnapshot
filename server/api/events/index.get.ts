adc6b715eabb9e3864875bb0c3a2ec247bffbd51
rinne <180847792+rinnein@users.noreply.github.com>
2026-02-21 17:43:55 +0800

    feat: add event listing API

---
+++ server/api/events/index.get.ts
@@ -0,0 +1,14 @@
+import { defineHandler, getValidatedQuery } from 'nitro/h3'
+import z from 'zod'
+import { Event } from '~/server/utils/common/event'
+
+export default defineHandler(async (event) => {
+  const query = await getValidatedQuery(
+    event,
+    z.object({
+      after: z.int().nonnegative().optional(),
+    }),
+  )
+  const events = await Event.listEvents(query.after)
+  return { events }
+})
