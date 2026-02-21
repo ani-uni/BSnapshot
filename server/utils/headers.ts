5fc822d6beca54404094fd78f789157732737387
rinne <180847792+rinnein@users.noreply.github.com>
2026-02-21 21:39:33 +0800

    refactor: convert headers to a class with static properties and a getter method

--- server/utils/headers.ts
+++ server/utils/headers.ts
@@ -1,13 +1,15 @@
 import pkg from '~/package.json'
-
-export const headers = {
-  bili_web: new Headers({
+export class headers {
+  static readonly bili_web = new Headers({
     'User-Agent':
       'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36',
     Referer: 'https://www.bilibili.com/',
     Origin: 'https://www.bilibili.com',
-  }),
-  app: new Headers({
+  })
+  static readonly app = new Headers({
     'User-Agent': `ani-uni/BSnapshot/${pkg.version} (${pkg.homepage})`,
-  }),
+  })
+  get(idn: keyof typeof headers) {
+    return headers[idn]
+  }
 }
