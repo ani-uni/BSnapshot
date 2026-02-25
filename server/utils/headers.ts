fe4e9637fe6b97c637b5bd7be9fd09cdf8e2652a
rinne <180847792+rinnein@users.noreply.github.com>
2026-02-25 14:22:45 +0800

    fix: update User-Agent string for bili_web header

--- server/utils/headers.ts
+++ server/utils/headers.ts
@@ -3,7 +3,7 @@ import pkg from '~/package.json'
 class HeadersClass {
   static readonly bili_web = new Headers({
     'User-Agent':
-      'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36',
+      'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36',
     Referer: 'https://www.bilibili.com/',
     Origin: 'https://www.bilibili.com',
   })
