a6b8598d2b825a799bc9a6aaf3a64fd7a1187986
rinne <180847792+rinnein@users.noreply.github.com>
2026-03-03 01:25:27 +0800

    fix: virtual default groups compatibility for episodes

--- server/api/episodes/index.get.ts
+++ server/api/episodes/index.get.ts
@@ -3,6 +3,6 @@ import { Episode } from '~s/utils/common/episode'
 
 // 用于获取全部的剧集，包含 有/无 所属季度状态
 export default defineHandler(async () => {
-  const episodes = await Episode.list(true)
+  const episodes = await Episode.list(false)
   return episodes
 })
