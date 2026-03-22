1568141f2d1722d7a441cde9df04d045324e22c4
rinne <180847792+rinnein@users.noreply.github.com>
2026-03-22 22:58:52 +0800

    feat: add user existence check to API handler

--- server/api/index.get.ts
+++ server/api/index.get.ts
@@ -1,11 +1,14 @@
 import { defineHandler } from 'nitro/h3'
 import { version as nitroVersion } from 'nitro/meta'
 import pkg from '~/package.json'
+import { prisma } from '~s/utils/prisma'
 
-export default defineHandler(() => {
+export default defineHandler(async () => {
+  const userExist = !((await prisma.user.findFirst({ select: {} })) !== null)
   return {
     name: pkg.name,
     version: pkg.version,
     nitroVersion,
+    userExist,
   }
 })
