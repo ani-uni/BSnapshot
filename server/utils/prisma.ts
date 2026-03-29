5f9e7a6238026684f799126cee68123e6cf53376
rinne <180847792+rinnein@users.noreply.github.com>
2026-03-29 15:00:18 +0800

    fix: update default database URL in Prisma adapter configuration

--- server/utils/prisma.ts
+++ server/utils/prisma.ts
@@ -3,7 +3,7 @@ import { PrismaLibSql } from '@prisma/adapter-libsql'
 import { PrismaClient } from '~/generated/prisma/client'
 
 const adapter = new PrismaLibSql({
-  url: process.env.DATABASE_URL ?? '',
+  url: process.env.DATABASE_URL ?? '.data/db/prisma.db',
 })
 
 const prisma = new PrismaClient({ adapter })
