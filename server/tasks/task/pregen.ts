f39344a20fd82213c8f08fb63ef0a499d3b4519a
rinne <180847792+rinnein@users.noreply.github.com>
2026-04-15 19:36:01 +0800

    feat: pregen supports b23.tv short link parse

--- server/tasks/task/pregen.ts
+++ server/tasks/task/pregen.ts
@@ -52,7 +52,7 @@ function abParser(
   return null
 }
 
-function inputParser(input: string) {
+async function inputParser(input: string) {
   let ab = abParser(input)
   if (ab) return ab
   // 尝试暴力匹配BVID
@@ -61,9 +61,22 @@ function inputParser(input: string) {
   if (ab) return ab
   // 接下来检测是否可识别为常见的链接形式
   try {
-    const url = new URL(input)
+    let url = new URL(decodeURIComponent(input))
+    // 若为b23.tv短链接，则需额外处理
+    if (url.hostname === 'b23.tv') {
+      const ps = url.pathname.split('/')
+      if (ps[0]) ab = abParser(ps[0])
+      if (ab) return ab
+      // 否则为生成的短链接，需解析重定向到的链接
+      const res = await fetch(url.toString(), {
+        method: 'GET',
+        redirect: 'manual',
+      })
+      const location = res.headers.get('Location')
+      if (location) url = new URL(location)
+    }
     // 尝试匹配 /video/BV1PWcXzrExh 或 /video/av12345678 或 无/video 等形式
-    const ps = url.pathname.split('/')
+    const ps = url.pathname.split('/').filter(Boolean)
     if (ps[0] === 'video' && ps[1]) ab = abParser(ps[1])
     else if (ps[0]) ab = abParser(ps[0])
     if (ab) return ab
@@ -99,7 +112,7 @@ export async function TaskPreGen(
       ],
     }
   } else {
-    const abid = inputParser(payload.id)
+    const abid = await inputParser(payload.id)
     const abBasic = getVideoBasic(
       await User.fromRotating(),
       abid.type === 'aid' ? { aid: abid.id } : { bvid: abid.id },
