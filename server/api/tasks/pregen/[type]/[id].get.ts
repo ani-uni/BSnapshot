054093449635d0cb37a94e97f7e20f9e14dff954
rinne <180847792+rinnein@users.noreply.github.com>
2026-04-15 18:59:36 +0800

    feat: pregen func supports bili link parse

--- server/api/tasks/pregen/[type]/[id].get.ts
+++ server/api/tasks/pregen/[type]/[id].get.ts
@@ -1,26 +1,11 @@
 import { defineHandler, getValidatedRouterParams } from 'nitro/h3'
-import z from 'zod'
-import { TaskPreGen } from '~s/tasks/task/pregen'
+import { TaskPreGen, TaskTaskPreGenPayloadSchema } from '~s/tasks/task/pregen'
 import { bigint2string } from '~s/utils/bigint'
-import { stringToBigInt } from '~s/utils/codecs'
 
 export default defineHandler(async (event) => {
   const payload = await getValidatedRouterParams(
     event,
-    z.discriminatedUnion('type', [
-      z.object({
-        type: z.literal('aid'),
-        id: stringToBigInt,
-      }),
-      z.object({
-        type: z.literal('bvid'),
-        id: z.string(),
-      }),
-      z.object({
-        type: z.literal('cid'),
-        id: stringToBigInt,
-      }),
-    ]),
+    TaskTaskPreGenPayloadSchema,
   )
   const res = await TaskPreGen(payload)
   return bigint2string(res)
--- server/tasks/task/pregen.ts
+++ server/tasks/task/pregen.ts
@@ -1,24 +1,24 @@
 import { HTTPError } from 'nitro/h3'
 import { defineTask } from 'nitro/task'
+import z from 'zod'
 import type { TaskPayload } from '~s/types/tasks/payload'
 import type { TaskResult } from '~s/types/tasks/result'
 import { bigint2string } from '~s/utils/bigint'
 import { getVideoBasic, type VideoBasic } from '~s/utils/bili/video/main'
+import { stringToBigInt } from '~s/utils/codecs'
 import { User } from '~s/utils/common/user'
 
-export type TaskTaskPreGenPayload =
-  | {
-      type: 'aid'
-      id: bigint
-    }
-  | {
-      type: 'bvid'
-      id: string
-    }
-  | {
-      type: 'cid'
-      id: bigint
-    }
+export const TaskTaskPreGenPayloadSchema = z.discriminatedUnion('type', [
+  z.object({
+    type: z.literal('auto'),
+    id: z.string(),
+  }),
+  z.object({
+    type: z.literal('cid'),
+    id: stringToBigInt,
+  }),
+])
+export type TaskTaskPreGenPayload = z.infer<typeof TaskTaskPreGenPayloadSchema>
 
 export type TaskTaskPreGenResult = VideoBasic
 
@@ -32,14 +32,51 @@ export default defineTask<TaskResult<TaskTaskPreGenResult>>({
       !payload.type ||
       !payload.id ||
       (payload.type === 'cid' && typeof payload.id !== 'bigint') ||
-      (payload.type === 'aid' && typeof payload.id !== 'bigint') ||
-      (payload.type === 'bvid' && typeof payload.id !== 'string')
+      (payload.type === 'auto' && typeof payload.id !== 'string')
     )
       throw new HTTPError('Invalid payload', { status: 400 })
     const res = await TaskPreGen(payload as TaskTaskPreGenPayload)
     return { result: bigint2string(res) }
   },
 })
+
+function abParser(
+  input: string,
+): { type: 'aid'; id: bigint } | { type: 'bvid'; id: string } | null {
+  if (z.coerce.bigint().safeParse(input).success)
+    return { type: 'aid', id: BigInt(input) }
+  else if (input.startsWith('BV') || input.startsWith('bv'))
+    return { type: 'bvid', id: input }
+  else if (input.startsWith('AV') || input.startsWith('av'))
+    return { type: 'aid', id: BigInt(input.slice(2)) }
+  return null
+}
+
+function inputParser(input: string) {
+  let ab = abParser(input)
+  if (ab) return ab
+  // 尝试暴力匹配BVID
+  const bvMatch = input.match(/BV[0-9A-Za-z]{10}/)
+  if (bvMatch) ab = abParser(bvMatch[0])
+  if (ab) return ab
+  // 接下来检测是否可识别为常见的链接形式
+  try {
+    const url = new URL(input)
+    // 尝试匹配 /video/BV1PWcXzrExh 或 /video/av12345678 或 无/video 等形式
+    const ps = url.pathname.split('/')
+    if (ps[0] === 'video' && ps[1]) ab = abParser(ps[1])
+    else if (ps[0]) ab = abParser(ps[0])
+    if (ab) return ab
+    // 尝试从查询参数中提取 aid/bvid
+    const said = url.searchParams.get('aid') || url.searchParams.get('oid')
+    if (said) ab = abParser(said)
+    if (ab) return ab
+    const sbvid = url.searchParams.get('bvid')
+    if (sbvid) ab = abParser(sbvid)
+    if (ab) return ab
+  } catch {}
+  throw new HTTPError('Invalid input format', { status: 400 })
+}
 
 export async function TaskPreGen(
   payload: TaskTaskPreGenPayload,
@@ -62,9 +99,10 @@ export async function TaskPreGen(
       ],
     }
   } else {
+    const abid = inputParser(payload.id)
     const abBasic = getVideoBasic(
       await User.fromRotating(),
-      payload.type === 'aid' ? { aid: payload.id } : { bvid: payload.id },
+      abid.type === 'aid' ? { aid: abid.id } : { bvid: abid.id },
       fastcap_manual,
     )
     return abBasic
