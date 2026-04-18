49820e227510238b927d1494c9eb3f4515fa0afd
rinne <180847792+rinnein@users.noreply.github.com>
2026-04-19 02:56:39 +0800

    fix: db-migrate

--- server/api/db/migrate.post.ts
+++ server/api/db/migrate.post.ts
@@ -1,15 +1,7 @@
-import { defineHandler, readValidatedBody } from 'nitro/h3'
-import z from 'zod'
+import { defineHandler } from 'nitro/h3'
 import { migrateDB } from '~s/utils/db-migrate'
 
-export default defineHandler(async (event) => {
-  const payload = await readValidatedBody(
-    event,
-    z.object({
-      s: z.union([z.string(), z.number()]).optional(),
-      e: z.union([z.string(), z.number()]).optional(),
-    }),
-  )
-  migrateDB(payload.s, payload.e)
+export default defineHandler(() => {
+  migrateDB()
   return { success: true }
 })
--- server/utils/db-migrate.ts
+++ server/utils/db-migrate.ts
@@ -1,9 +1,9 @@
 import 'dotenv/config'
+import fs from 'node:fs'
 import path from 'node:path'
 
 import Database from 'libsql'
 
-import pkg from '~/package.json' with { type: 'json' }
 import init_sql from '~/prisma/migrations/0_init/migration.sql?raw'
 import add_video_source_sql from '~/prisma/migrations/20260408153716_add_video_source/migration.sql?raw'
 import sn_may_be_float_sql from '~/prisma/migrations/20260411152213_sn_may_be_float/migration.sql?raw'
@@ -13,11 +13,20 @@ const dbPath =
   (process.env.USER_DATA_PATH
     ? `file:${path.resolve(process.env.USER_DATA_PATH, '.data/db/prisma.db')}`
     : 'file:.data/db/prisma.db')
+const dbMigrationLockPath = process.env.USER_DATA_PATH
+  ? path.resolve(process.env.USER_DATA_PATH, '.data/db/prisma.migration-lock')
+  : '.data/db/prisma.migration-lock'
 
 const db2ver = {
   '0.0.1': 0,
   '0.0.3': 1,
   '0.0.7': 2,
+} as const
+
+const sqls = {
+  '0_init': init_sql,
+  '20260408153716_add_video_source': add_video_source_sql,
+  '20260411152213_sn_may_be_float': sn_may_be_float_sql,
 } as const
 
 const ver2db = (ver: string) => {
@@ -30,29 +39,30 @@ const ver2db = (ver: string) => {
   return -1
 }
 
-const migrateDB = (
-  s: number | string = -1,
-  e: number | string = pkg.version,
-) => {
-  if (!e) e = s
-  if (typeof s === 'string') s = db2ver[s as keyof typeof db2ver] ?? 0
-  if (typeof e === 'string') e = db2ver[e as keyof typeof db2ver] ?? 0
+const migrateDB = () => {
+  if (!fs.existsSync(dbMigrationLockPath)) {
+    fs.mkdirSync(path.dirname(dbMigrationLockPath), { recursive: true })
+    fs.writeFileSync(dbMigrationLockPath, '[]')
+  }
   const db = new Database(dbPath)
-  for (let v = s + 1; v <= e; v++) {
-    switch (v) {
-      case 0:
-        db.exec(init_sql)
-        break
-      case 1:
-        db.exec(add_video_source_sql)
-        break
-      case 2:
-        db.exec(sn_may_be_float_sql)
-        break
-      default:
-        break
+  let done_raw = fs.readFileSync(dbMigrationLockPath, 'utf-8')
+  if (done_raw === '') done_raw = '[]'
+  try {
+    JSON.parse(done_raw)
+  } catch {
+    done_raw = '[]'
+  }
+  const done = new Set<keyof typeof sqls>(JSON.parse(done_raw) ?? [])
+  const execMigrate = (migration: keyof typeof sqls) => {
+    if (!done.has(migration)) {
+      db.exec(sqls[migration])
+      done.add(migration)
     }
   }
+  for (const migration of Object.keys(sqls) as (keyof typeof sqls)[]) {
+    execMigrate(migration)
+  }
+  fs.writeFileSync(dbMigrationLockPath, JSON.stringify([...done]))
   db.close()
 }
 
